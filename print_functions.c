0872ad8ada50f91866e448e74193a33d099017c0
Imad Jhabli <imadjhabli@gmail.com>
2023-07-23 08:42:02 -0700

    update

--- print_functions.c
+++ print_functions.c
@@ -89,30 +89,3 @@ int print_integer(va_list args)
 
 	return (count);
 }
-/**
- * print_decimal - Prints a decimal to stdout
- * @args: A va_list containing the arguments to be printed
- * Return: The number of characters printed
- */
-int print_decimal(va_list args)
-{
-	int n = va_arg(args, int);
-	int count = 0;
-	/* Implementation for handling 'd' (same as print_integer) */
-	if (n < 0)
-	{
-		count += _putchar('-');
-		if (n == -2147483648)
-		{
-			count += _putchar('2');
-			n %= 1000000000;
-		}
-		count += print_decimal(-n);
-		}
-		else
-		{
-			count += print_decimal(n / 10);
-			count += _putchar((n % 10) + '0');
-		}
-		return (count);
-}
